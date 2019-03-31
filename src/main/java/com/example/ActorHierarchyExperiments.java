88aa7c90c9bc742a2b93c89466f54f5eb9dfbd48
syk-coder <raghavender.m@flipkart.com>
2019-03-31 23:26:10 +0530

    akka actorRef experiment-post and pre stop

--- src/main/java/com/example/ActorHierarchyExperiments.java
+++ src/main/java/com/example/ActorHierarchyExperiments.java
@@ -33,10 +33,67 @@ public class ActorHierarchyExperiments {
         firstRef.tell("printit", ActorRef.noSender());
 
         System.out.println(">>> Press ENTER to exit <<<");
+
+
+        ActorRef first = system.actorOf(StartStopActor1.props(), "first");
+        first.tell("stop", ActorRef.noSender());
+
         try {
             System.in.read();
         } finally {
             system.terminate();
         }
+    }
+}
+
+class StartStopActor1 extends AbstractActor {
+    static Props props() {
+        return Props.create(StartStopActor1.class, StartStopActor1::new);
+    }
+
+    @Override
+    public void preStart() {
+        System.out.println("first started");
+        getContext().actorOf(StartStopActor2.props(), "second");
+    }
+
+    @Override
+    public void postStop() {
+        System.out.println("first stopped");
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals(
+                        "stop",
+                        s -> {
+                            getContext().stop(getSelf());
+                        })
+                .build();
+    }
+}
+
+class StartStopActor2 extends AbstractActor {
+
+    static Props props() {
+        return Props.create(StartStopActor2.class, StartStopActor2::new);
+    }
+
+    @Override
+    public void preStart() {
+        System.out.println("second started");
+    }
+
+    @Override
+    public void postStop() {
+        System.out.println("second stopped");
+    }
+
+    // Actor.emptyBehavior is a useful placeholder when we don't
+    // want to handle any messages in the actor.
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder().build();
     }
 }
