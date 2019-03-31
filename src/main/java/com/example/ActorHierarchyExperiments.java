106e01c57371d49bd21aec983ea006888b493bf8
syk-coder <raghavender.m@flipkart.com>
2019-03-31 20:16:19 +0530

    akka actorRef experiment

--- src/main/java/com/example/ActorHierarchyExperiments.java
+++ src/main/java/com/example/ActorHierarchyExperiments.java
@@ -1,29 +1,42 @@
 package com.example;
 
 import akka.actor.AbstractActor;
+import akka.actor.AbstractActor.Receive;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 
-public class ActorHierarchyExperiments {
-
-    public static void main(String[] args) {
-        ActorSystem actorSystem = ActorSystem.create("testActor");
-
-
+class PrintMyActorRefActor extends AbstractActor {
+    static Props props() {
+        return Props.create(PrintMyActorRefActor.class, PrintMyActorRefActor::new);
     }
 
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals(
+                        "printit",
+                        p -> {
+                            ActorRef secondRef = getContext().actorOf(Props.empty(), "second-actor");
+                            System.out.println("Second: " + secondRef);
+                        })
+                .build();
+    }
+}
+public class ActorHierarchyExperiments {
+    public static void main(String[] args) throws java.io.IOException {
+        ActorSystem system = ActorSystem.create("testSystem");
 
+        System.out.println("System: "+system);
+        ActorRef firstRef = system.actorOf(PrintMyActorRefActor.props(), "first-actor");
+        System.out.println("First: " + firstRef);
+        firstRef.tell("printit", ActorRef.noSender());
 
-    class PrintMyActorRefActor extends AbstractActor {
-
-        static Props props(){
-            return Props.create(PrintMyActorRefActor.class, PrintMyActorRefActor::new);
-
-        }
-
-        @Override
-        public Receive createReceive() {
-            return null;
+        System.out.println(">>> Press ENTER to exit <<<");
+        try {
+            System.in.read();
+        } finally {
+            system.terminate();
         }
     }
 }
