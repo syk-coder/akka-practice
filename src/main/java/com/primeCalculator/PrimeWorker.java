0bc6ac3ffeef30af5652cd09f32838276454b709
syk-coder <raghavender.m@flipkart.com>
2019-04-01 16:13:14 +0530

    SPrime Calculator - Added worker

---
+++ src/main/java/com/primeCalculator/PrimeWorker.java
@@ -0,0 +1,36 @@
+package com.primeCalculator;
+
+import akka.actor.UntypedActor;
+
+public class PrimeWorker extends UntypedActor{
+    @Override
+    public void onReceive(Object message) throws Throwable {
+        if (message instanceof NumberRangeMessage){
+
+            NumberRangeMessage numberRangeMessage = (NumberRangeMessage) message;
+            System.out.println("Recieved the number range from: "+numberRangeMessage.getStartNumber()+" to: "+((NumberRangeMessage) message).getEndNumber());
+
+            Result result = new Result();
+
+            for (long l = numberRangeMessage.getStartNumber(); l < numberRangeMessage.getEndNumber(); l++) {
+                if (isPrime(l)){
+                    
+                }
+            }
+
+
+        }
+    }
+
+    private boolean isPrime(long l) {
+        if (l == 1 || l==2 || l==3) return true;
+        else if(l %2 == 0){
+            return false;
+        }
+
+        for (long i = 3 ; i < l ; i++ ){
+            if (l %i == 0) return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/primeCalculator/Result.java
@@ -0,0 +1,14 @@
+package com.primeCalculator;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class Result {
+
+    private List<Long> results = new ArrayList<>();
+
+
+}
